469c0e6dda800fd79a3a00eb768836f7720c6f5c
Ariel Arevalo <Ariel.Arevalo@datasite.com>
2024-10-03 21:07:07 -0600

    #32: Agrega firma para funcion de busqueda

--- tarea_1/health_topic_index/view/__init__.py
+++ tarea_1/health_topic_index/view/__init__.py
@@ -1,5 +1,5 @@
 from .ui import UI
 
 from .pages.main_page import display_main_page
-from .pages.stadistics_pages import display_data_statistics_page
+from .pages.statistics_pages import display_data_statistics_page
 from .pages.tables_pages import display_health_themes_page, display_sites_page
