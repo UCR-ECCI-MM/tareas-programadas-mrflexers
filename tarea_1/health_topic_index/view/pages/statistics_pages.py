82d62abb5a031684b684c90d6a10c4979444e9db
JirlOP <jorge.diazsagot@ucr.ac.cr>
2024-10-04 18:30:30 -0600

    Editado el grafico de la pagina display_data_statistics_page para lucir mejor

--- tarea_1/health_topic_index/view/pages/statistics_pages.py
+++ tarea_1/health_topic_index/view/pages/statistics_pages.py
@@ -1,10 +1,30 @@
+from io import BytesIO
+import altair as alt
 import streamlit as st
-
 
 def display_data_statistics_page():
     st.title("Estadísticas")
 
     if 'dataset' in st.session_state:
+        # obtain from the dataset the top information categories
         df = st.session_state.dataset.get_top_info_cat()
+
+        value_column = df.columns[1]
+        label_column = df.columns[0]
+
         st.write('## Categorías de Información más populares')
-        st.bar_chart(df, x_label='Categoría de Información', y_label='Conteo de Temas')
+
+        # Create the bar chart
+        bar_chart = alt.Chart(df).mark_bar().encode(
+            x=alt.X(f'{value_column}:Q', title='Conteo de Temas'),
+            y=alt.Y(f'{label_column}:N', title='Categoría de Información', sort='-x'),
+            tooltip=[label_column, value_column]  # to show details on hover
+        ).configure_axis(
+            labelLimit=200,  # Adjust the limit of large labels
+        ).properties(
+            height=500  # Height of the chart
+        )
+
+        # use on_select when the graph need to be dynamic
+        st.altair_chart(bar_chart, use_container_width=True)
+
